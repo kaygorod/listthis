9f94420ecc2c085a8fb42721ce273eaafc51ee00
«kaygorod» <kaygorod@gmail.com>
2016-06-20 13:20:42 +0300

    ihor

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,79 +5,28 @@ lock '3.5.0'
 # Project configuration options
 # ------------------------------
 
-set :application,    'ListThis'
-set :login,          'kaygorod'
-set :user,           'hosting_kaygorod'
+set :application, 'app'
+set :repo_url, "git@github.com:kaygorod/listthis.git"
+set :deploy_to, "/var/www/#{fetch(:application)}"
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/uploads')
+set :bundle_binstubs, -> { shared_path.join('bin') }
+set :keep_releases, 3
 
-set :deploy_to,      "/home/#{fetch(:user)}/projects/#{fetch(:application)}"
-set :unicorn_conf,   "/etc/unicorn/#{fetch(:application)}.#{fetch(:login)}.rb"
-set :unicorn_pid,    "/var/run/unicorn/#{fetch(:user)}/" \
-                     "#{fetch(:application)}.#{fetch(:login)}.pid"
-set :bundle_without, %w{development test}.join(' ')             # this is default
-set :use_sudo,       false
-
-set :repo_url,       "git@github.com:kaygorod/listthis.git"
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-set :scm, :git
-set :format, :pretty
-set :pty, true
-
-# Change the verbosity level
-set :log_level, :info
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w(bin log tmp/cache tmp/pids vendor/bundle public/system)
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Configure RVM
-set :rvm_ruby_version, '2.2.4'
-
-# You unlikely have to change below this line
-# -----------------------------------------------------------------------------
-
-# Configure RVM
-set :rake,            "rvm use #{fetch(:rvm_ruby_version)} do bundle exec rake"
-set :bundle_cmd,      "rvm use #{fetch(:rvm_ruby_version)} do bundle"
-
-set :assets_roles, [:web, :app]
-
-set :unicorn_start_cmd,
-    "(cd #{fetch(:deploy_to)}/current; rvm use #{fetch(:rvm_ruby_version)} " \
-    "do bundle exec unicorn_rails -Dc #{fetch(:unicorn_conf)})"
-
-# - for unicorn - #
 namespace :deploy do
-  desc 'Start application'
-  task :start do
-    on roles(:app) do
-      execute unicorn_start_cmd
+  %w[unicorn resque].each do |service|
+    namespace service do
+      %w[up down restart status].each do |command|
+        desc "#{command.capitalize} #{service}"
+        task command do
+          on roles(:app) do
+            execute "sudo sv #{command} #{fetch(:application)}_#{service}"
+          end
+        end
+      end
     end
   end
 
-  desc 'Stop application'
-  task :stop do
-    on roles(:app) do
-      execute "[ -f #{fetch(:unicorn_pid)} ] && " \
-              "kill -QUIT `cat #{fetch(:unicorn_pid)}`"
-    end
-  end
-
-  after :publishing, :restart
-
-  desc 'Restart Application'
-  task :restart do
-    on roles(:app) do
-      execute "[ -f #{fetch(:unicorn_pid)} ] && " \
-              "kill -USR2 `cat #{fetch(:unicorn_pid)}` || " \
-              "#{fetch(:unicorn_start_cmd)}"
-    end
-  end
+  after :finished, 'unicorn:restart'
+  after :finished, 'resque:restart'
 end
