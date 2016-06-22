079915fa2f94288ef97a24f9a0846b347a5cd8fa
«kaygorod» <kaygorod@gmail.com>
2016-06-22 13:24:25 +0300

    ihor

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
+server '194.67.201.84', user: 'deploy', roles: %w{web app db}
 
 
 
