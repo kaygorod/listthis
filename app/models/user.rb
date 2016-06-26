550376e61f00c481f04950e52a26c93d62c49eb1
«kaygorod» <kaygorod@gmail.com>
2016-06-26 12:26:55 +0300

    devise mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  def to_param
-    username
-  end
+         :recoverable, :rememberable, :trackable,
+         :validatable,:confirmable
 end
