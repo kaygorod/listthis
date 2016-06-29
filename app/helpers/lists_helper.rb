cb6a7f6797969008f81e7a6c6c5af67194ea0bcc
«kaygorod» <kaygorod@gmail.com>
2016-06-29 12:29:12 +0300

    user item

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,7 +1,6 @@
 module ListsHelper
   def authors_items
-    Item.where("list_id = ?", @list.id).group(:user_id)
-
+    Item.where("list_id = ?", @list.id).select("user_id").group("user_id")
 
   end
 
