488740dee0c1d91a9822eae3c1204762bb28f927
«kaygorod» <kaygorod@gmail.com>
2016-08-25 22:29:19 +0300

    devise modal and ios zoom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,15 @@ def after_sign_out_path_for(resource_or_scope)
   request.referrer
 end
 
+def after_sign_in_path_for(resource)
+  sign_in_url = new_user_session_url
+  if request.referer == sign_in_url
+    super
+  else
+    stored_location_for(resource) || request.referer || root_path
+  end
+end
+
   protected
   def store_current_location
     store_location_for(:user, request.url)
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   #end
 
   devise_for :users, path_names: {sign_in:"login", sign_out:"logout"},
-                     :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+                     :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",
+                                       :sessions => 'sessions',
+                                       :registrations => 'registrations'}
+
   get 'static_pages/help'
   resources :lists do
     member do
