ea9cf7981c108c74f33aedfa09bfd6f4781bc0ac
«kaygorod» <kaygorod@gmail.com>
2016-08-29 13:15:27 +0300

    fix

--- config/deploy/locum.rb
+++
@@ -1,6 +0,0 @@
-role :app, %w(hosting_kaygorod@chromium.locum.ru)
-role :web, %w(hosting_kaygorod@chromium.locum.ru)
-role :db, %w(hosting_kaygorod@chromium.locum.ru)
-
-set :ssh_options, forward_agent: true
-set :rails_env, :production
